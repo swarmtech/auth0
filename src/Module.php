3835d35475243423d0a1ca8ab04342b1aac17d2f
Gary Gitton <gary.gitton@gmail.com>
2019-12-08 01:25:29 +0100

    add module file

---
+++ src/Module.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Swarmtech\Auth0;
+
+use Swarmtech\Auth0\MvcAuth\Adapter\AuthenticationAdapter;
+use Zend\Mvc\MvcEvent;
+use ZF\MvcAuth\Authentication\DefaultAuthenticationListener;
+
+/**
+ * Class Module
+ * @package Swarmtech\Auth0
+ */
+final class Module
+{
+    public function getConfig(): array
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $mvcEvent)
+    {
+        $application = $mvcEvent->getApplication();
+        $events = $application->getEventManager();
+        $container = $application->getServiceManager();
+
+        $events->attach(
+            'authentication',
+            function ($mvcEvent) use ($container) {
+                $listener = $container->get(DefaultAuthenticationListener::class);
+                $adapter = $container->get(AuthenticationAdapter::class);
+                $listener->attach($adapter);
+            },
+            1000
+        );
+    }
+}
